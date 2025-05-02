6efe3bca54466a0e884310d1e94774bd42887dca
Andile763 <sibandamishell@gmail.com>
2025-05-02 11:41:33 +0200

    Added basic html form, typewriter,  changed  container

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault(); // Prevent the form from submitting
 
+  new Typewriter("#recipe", {
+    string: ["Generating your breakfast recipe..."],
+    autoStart: true,
+    cursor: null,
+    //delay: 200,
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
