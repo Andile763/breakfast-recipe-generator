f707aacc06c866446ae92881334142011fdeecb9
Andile763 <sibandamishell@gmail.com>
2025-05-03 13:24:09 +0200

    Added API

--- src/script.js
+++ src/script.js
@@ -1,12 +1,27 @@
+function displayRecipe(response) {
+  console.log("recipe generated", response.data.answer);
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault(); // Prevent the form from submitting
 
-  new Typewriter("#recipe", {
-    string: ["Generating your breakfast recipe..."],
-    autoStart: true,
-    cursor: null,
-    //delay: 200,
-  });
+  let ingredientsInput = document.querySelector("#user-input");
+  let context =
+    "You're the best chef in the world. You are a breakfast recipe generator. Your mission is to generate any 3-ingredients breakfast recipe based on the ingredient provided. The recipe should be clear,concise and easy to follow. The recipe should include the name of the dish, the ingredients, and the steps to prepare it. The recipe should be in English. The recipe should be suitable for a beginner cook. The recipe should be healthy and delicious.Generate this in basic HTML and don't write html then follow with the recipe, just write the recipe. Sign SheCodes AI at the end of the recipe. Make SheCodes AI blue like the search-button";
+  let prompt = `User input:Generate a breakfast recipe about ${ingredientsInput.value}`;
+
+  let apiKey = "dc027835t447909af572ffob036660a3";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe...");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-form");
